4e04fcd16411465d79e9f98c7b08c7b953f899da
santinoMatIm <santinomi1306@gmail.com>
2025-11-28 18:51:49 -0600

    changed car-2024 orientation and traffic light road

--- AgentsVisualization/Server/trafficServer/trafficBase/model.py
+++ AgentsVisualization/Server/trafficServer/trafficBase/model.py
@@ -12,7 +12,7 @@ class CityModel(Model):
     """
     def __init__(self, N):
         super().__init__()
-        
+
         # Load the map dictionary. The dictionary maps the characters in the map file to the corresponding agent.
         dataDictionary = json.load(open("city_files/mapDictionary.json"))
 
@@ -24,7 +24,7 @@ class CityModel(Model):
             self.width = len(lines[0])-1
             self.height = len(lines)
 
-            self.grid = MultiGrid(self.width, self.height, torus = False) 
+            self.grid = MultiGrid(self.width, self.height, torus = False)
 
             # Goes through each character in the map file and creates the corresponding agent.
             for r, row in enumerate(lines):
@@ -34,6 +34,14 @@ class CityModel(Model):
                         self.grid.place_agent(agent, (c, self.height - r - 1))
 
                     elif col in ["S", "s"]:
+                        # Determine road direction for traffic light by checking neighbors
+                        road_direction = self._get_road_direction_for_traffic_light(lines, r, c, dataDictionary)
+
+                        # Place a road under the traffic light so cars can pass through
+                        road_agent = Road(f"r_{r*self.width+c}", self, road_direction)
+                        self.grid.place_agent(road_agent, (c, self.height - r - 1))
+
+                        # Place traffic light on top of the road
                         agent = Traffic_Light(f"tl_{r*self.width+c}", self, False if col == "S" else True, int(dataDictionary[col]))
                         self.grid.place_agent(agent, (c, self.height - r - 1))
                         self.traffic_lights.append(agent)
@@ -70,6 +78,58 @@ class CityModel(Model):
                     break
 
         self.running = True
+
+    def _get_road_direction_for_traffic_light(self, lines, r, c, dataDictionary):
+        """
+        Determines the road direction for a traffic light by checking neighboring cells.
+        Returns: "Up", "Down", "Left", or "Right"
+        """
+        # Check all four neighbors to find road directions
+        height = len(lines)
+        width = len(lines[0]) - 1
+
+        # Check left neighbor
+        if c > 0 and lines[r][c-1] in ["v", "^", ">", "<"]:
+            neighbor_dir = dataDictionary[lines[r][c-1]]
+            # If left neighbor points right, this road should go right
+            if neighbor_dir == "Right":
+                return "Right"
+            # If left neighbor points left, this road should go left
+            if neighbor_dir == "Left":
+                return "Left"
+
+        # Check right neighbor
+        if c < width - 1 and lines[r][c+1] in ["v", "^", ">", "<"]:
+            neighbor_dir = dataDictionary[lines[r][c+1]]
+            # If right neighbor points left, this road should go left
+            if neighbor_dir == "Left":
+                return "Left"
+            # If right neighbor points right, this road should go right
+            if neighbor_dir == "Right":
+                return "Right"
+
+        # Check top neighbor (r-1 because text file is top-down)
+        if r > 0 and lines[r-1][c] in ["v", "^", ">", "<"]:
+            neighbor_dir = dataDictionary[lines[r-1][c]]
+            # If top neighbor points down, this road should go down
+            if neighbor_dir == "Down":
+                return "Down"
+            # If top neighbor points up, this road should go up
+            if neighbor_dir == "Up":
+                return "Up"
+
+        # Check bottom neighbor
+        if r < height - 1 and lines[r+1][c] in ["v", "^", ">", "<"]:
+            neighbor_dir = dataDictionary[lines[r+1][c]]
+            # If bottom neighbor points up, this road should go up
+            if neighbor_dir == "Up":
+                return "Up"
+            # If bottom neighbor points down, this road should go down
+            if neighbor_dir == "Down":
+                return "Down"
+
+        # Default to "Right" if no neighbors found
+        return "Right"
 
     def step(self):
         '''Advance the model by one step.'''
